5dda417d4652d5028a9094512f84453251e2631f
vigo2013 <fw121fw4@163.com>
2014-11-25 15:12:09 +0800

    项目初始化

---
+++ examw-test-app-core/src/com/examw/test/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.examw.test.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 主界面
+ * @author fengwei.
+ * @since 2014年9月3日 下午3:18:19.
+ */
+public class MainActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
